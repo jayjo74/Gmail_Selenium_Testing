a3e815bf64f06dd882ea24adadaf80a38982de2c
JAY-DELL\jayjo <iljejo74@gmail.com>
2016-09-01 23:09:09 -0700

    Create gmailLoginShouldBeSuccessful() testing scenario

--- src/test/java/Gmail_Testing.java
+++ src/test/java/Gmail_Testing.java
@@ -1,7 +1,12 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -15,6 +20,7 @@ public class Gmail_Testing {
     @Before
     public void beforeTesting(){
        driver = new FirefoxDriver();
+       driver.manage().window().maximize();
        wait = new WebDriverWait(driver,30);
     }
 
@@ -23,13 +29,51 @@ public class Gmail_Testing {
 
     // 1.Go to Gmail website
         driver.get("http://gmail.com");
-        
+
     // 2. Fill in userame
-    // 3. Fill in password
-    // 4. Click sign in
+        WebElement inputEmail = driver.findElement(By.cssSelector("input[id='Email']"));
+        wait.until(ExpectedConditions.visibilityOf(inputEmail));
+        inputEmail.clear();
+        inputEmail.sendKeys("iljejo74@gmail.com");
+
+    // 3. Click Next button
+        WebElement nextButton = driver.findElement(By.id("next"));
+        nextButton.click();
+
+    // 4. Fill in password
+//        WebElement inputPassword = driver.findElement(By.id("Passwd"));
+//        wait.until(ExpectedConditions.visibilityOf(inputPassword));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
+        driver.findElement(By.id("Passwd")).clear();
+        driver.findElement(By.id("Passwd")).sendKeys("Anselmo777");
+//        inputPassword.sendKeys("Anselmo777");
+
+    // 5. Click sign in
+        WebElement SignInButton = driver.findElement(By.cssSelector("input[id='signIn'][class='rc-button rc-button-submit']"));
+        SignInButton.click();
+
     // 5. Verify user did sign in
+        WebElement inboxText = driver.findElement(By.partialLinkText("Inbox"));
+        wait.until(ExpectedConditions.visibilityOf(inboxText));
+        Assert.assertTrue("Inbox is come out",driver.findElements(By.partialLinkText("Inbox")).size()>0);
+
     // 6. Sigh out
+        WebElement userChangeButton = driver.findElement(By.cssSelector("span[class='gb_3a gbii']"));
+        userChangeButton.click();
+
+        WebElement sighOutButton = driver.findElement(By.linkText("Sign out"));
+//        wait.until(ExpectedConditions.visibilityOf(sighOutButton));
+        sighOutButton.click();
+
+
+
+
     // 7. Verify user did sign out
+    }
+
+    @After
+    public void afterTesting(){
+        driver.quit();
     }
 
 }
